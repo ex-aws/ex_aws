5a095968d4dbbccd26906e9234b74a7713ae0aaa
Ben Wilson <benwilson512@gmail.com>
2014-09-16 13:01:33 -0400

    update config tests

--- lib/ex_aws/config.ex
+++ lib/ex_aws/config.ex
@@ -50,9 +50,7 @@ defmodule ExAws.Config do
 
   def namespace(data, :dynamo) do
     if table = Keyword.get(data, :TableName) do
-      name = [table, Application.get_env(:ex_aws, :ddb_namespace), Mix.env]
-        |> Enum.filter(&(&1))
-        |> Enum.join("_")
+      name = prefix([table, Application.get_env(:ex_aws, :ddb_namespace), Mix.env])
       Keyword.put(data, :TableName, name)
     else
       data
@@ -61,13 +59,15 @@ defmodule ExAws.Config do
 
   def namespace(data, :kinesis) do
     if table = Keyword.get(data, :StreamName) do
-      name = [table, Application.get_env(:ex_aws, :kinesis_namespace), Mix.env]
-        |> Enum.filter(&(&1))
-        |> Enum.join("_")
+      name = prefix([table, Application.get_env(:ex_aws, :kinesis_namespace), Mix.env])
       Keyword.put(data, :StreamName, name)
     else
       data
     end
+  end
+
+  defp prefix(items) when is_list(items) do
+    items |> Enum.filter(&(&1)) |> Enum.join("_")
   end
 
 end
--- test/lib/config_test.exs
+++ test/lib/config_test.exs
@@ -2,8 +2,8 @@ defmodule ExAws.ConfigTest do
   alias ExAws.Config
   use ExUnit.Case, async: true
 
-  test "#namespace_table" do
-    name = [TableName: "Foo"] |> Config.namespace_table
+  test "#namespace" do
+    name = [TableName: "Foo"] |> Config.namespace(:dynamo)
     assert name == [TableName: "Foo_test"]
   end
 end
