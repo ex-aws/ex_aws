9f9d94a8fcf72df56cf3c5eb9b8213dde3a1e11b
Ben Wilson <benwilson512@gmail.com>
2014-09-16 17:35:35 -0400

    wrap up kinesis actions

--- lib/ex_aws/kinesis.ex
+++ lib/ex_aws/kinesis.ex
@@ -2,8 +2,18 @@ defmodule ExAws.Kinesis do
   alias __MODULE__
   alias ExAws.Config
 
+  ## Streams
+  ######################
+
   def list_streams do
     request(:list_streams)
+  end
+
+  def describe_stream(name, opts \\ %{}) do
+    data = %{
+      StreamName: name
+    } |> Map.merge(opts)
+    request(:describe_stream, data)
   end
 
   def create_stream(name, shard_count \\ 1) do
@@ -11,7 +21,6 @@ defmodule ExAws.Kinesis do
       ShardCount: shard_count,
       StreamName: name
     }
-    data |> IO.inspect
     request(:create_stream, data)
   end
 
@@ -21,13 +30,79 @@ defmodule ExAws.Kinesis do
     request(:delete_stream, data)
   end
 
-  def put_record(stream_name, partition_key, blob, opts \\ []) do
-    data = [
+  ## Records
+  ######################
+
+  def get_records(shard_iterator, opts \\ %{}) do
+    data = %{
+      ShardIterator: shard_iterator
+    } |> Map.merge(opts)
+    request(:get_records, data)
+  end
+
+  def put_record(stream_name, partition_key, blob, opts \\ %{}) do
+    data = %{
       Data: blob,
       PartitionKey: partition_key,
       StreamName: stream_name
-    ] ++ opts
+    } |> Map.merge(opts)
     request(:put_record, data)
+  end
+
+  ## Shards
+  ######################
+
+  def get_shard_iterator(name, shard_id, shard_iterator_type, opts \\ %{}) do
+    data = %{
+      StreamName: name,
+      ShardId: shard_id,
+      ShardIteratorType: shard_iterator_type
+    } |> Map.merge(opts)
+    request(:get_shard_iterator, data)
+  end
+
+  def merge_shards(name, adjacent_shard, shard) do
+    data = %{
+      StreamName: name,
+      AdjacentShardToMerge: adjacent_shard,
+      ShardToMerge: shard
+    }
+    request(:merge_shards, data)
+  end
+
+  def split_shard(name, shard, new_starting_hash_key) do
+    data = %{
+      StreamName: name,
+      ShardToSplit: shard,
+      NewStartingHashKey: new_starting_hash_key
+    }
+    request(:split_shard, data)
+  end
+
+  ## Tags
+  ######################
+
+  def add_tags_to_stream(name, tags = %{}) do
+    data = %{
+      StreamName: name,
+      Tags: tags
+    }
+    request(:add_tags_to_stream, data)
+  end
+
+  def list_tags_for_stream(name, opts \\ %{}) do
+    data = %{
+      StreamName: name
+    } |> Map.merge(opts)
+    request(:list_tags_for_stream, data)
+  end
+
+  def remove_tags_for_stream(name, tag_keys) when is_list(tag_keys) do
+    data = %{
+      StreamName: name,
+      TagKeys: tag_keys
+    } |> Map.merge(opts)
+    request(:remove_tags_for_stream, data)
   end
 
   # This function should be used for everything.
