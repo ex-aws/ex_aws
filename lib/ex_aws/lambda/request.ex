7b8e75db482f270d79a4f977e39e32c8c08ddbd0
Ben Wilson <benwilson512@gmail.com>
2015-04-16 13:53:38 -0400

    fix lambda regression

--- lib/ex_aws/lambda/request.ex
+++ lib/ex_aws/lambda/request.ex
@@ -4,14 +4,14 @@ defmodule ExAws.Lambda.Request do
     path = [path, params |> URI.encode_query] |> IO.iodata_to_binary
 
     headers = [{"content-type", "application/json"} | headers]
-    ExAws.Request.request(http_method, adapter.config |> url, data, headers , adapter)
+    ExAws.Request.request(http_method, adapter.config |> url(path), data, headers , adapter)
   end
 
-  defp url(config) do
+  defp url(config, path) do
     [
       Keyword.get(config, :scheme),
       Keyword.get(config, :host),
-      "/"
+      path
     ] |> IO.iodata_to_binary
   end
 end
