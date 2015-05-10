23df11399bdae5e0cd1a793be0558c1deff8eba9
Rodney Norris <rnorris@myemma.com>
2015-05-09 23:55:56 -0500

    request work

--- lib/ex_aws/sqs/request.ex
+++ lib/ex_aws/sqs/request.ex
@@ -2,7 +2,24 @@ defmodule ExAws.SQS.Request do
   @moduledoc false
 
   def request(client, queue_name, action, params \\ []) do
-    {:error, "Not implemented"}  
+    {_, http_method} = ExAws.SQS.Impl |> ExAws.Actions.get(action)
+
+    headers = [
+    ]
+
+    ExAws.Request.request(http_method, client.config |> url(queue_name, params), [], headers, client)
+    |> parse(client.config)
+  end
+  
+  def parse({:error, result}, _), do: {:error, result}
+  def parse({:ok, body}, config) do
+    case config[:json_codec].decode(body) do
+      {:ok, result} -> {:ok, result}
+      {:error, _} -> {:error, body}
+    end
   end
 
+  def url(config, queue_name, params) do
+    ""
+  end
 end
