715b31771bdeb7817eb24752a55810255347b5a3
Rodney Norris <rnorris@myemma.com>
2015-05-16 13:03:19 -0500

    WIP - request

--- lib/ex_aws/sqs/request.ex
+++ lib/ex_aws/sqs/request.ex
@@ -5,12 +5,13 @@ defmodule ExAws.SQS.Request do
     {_, http_method} = ExAws.SQS.Impl |> ExAws.Actions.get(action)
 
     headers = [
+      {"content-type", "application/x-www-form-urlencoded"},
     ]
 
     ExAws.Request.request(http_method, client.config |> url(queue_name, params), [], headers, client)
     |> parse(client.config)
   end
-  
+
   def parse({:error, result}, _), do: {:error, result}
   def parse({:ok, body}, config) do
     case config[:json_codec].decode(body) do
@@ -20,6 +21,8 @@ defmodule ExAws.SQS.Request do
   end
 
   def url(config, queue_name, params) do
+    # https://sqs.[region].amazonaws.com/[account_id]/[queue_name]?
+    # Action=[action]&  
     ""
   end
 end
