808ca547f27a9899b48f7f9fe2e605f98cb102a1
Ben Wilson <benwilson512@gmail.com>
2015-05-18 16:26:05 -0400

    update version to v0.2.0-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExAws.Mixfile do
 
   def project do
     [app: :ex_aws,
-     version: "0.1.2",
+     version: "0.2.0-dev",
      elixir: "~> 1.0",
      description: "AWS client. Currently supports Dynamo, Kinesis, Lambda, S3",
      name: "ExAws",
