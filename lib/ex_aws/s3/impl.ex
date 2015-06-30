57e3195e7de9fd0da3ce125fd92e140d3c48c70a
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2015-06-30 17:12:09 +0200

    Make use of keyword lists

--- lib/ex_aws/s3/impl.ex
+++ lib/ex_aws/s3/impl.ex
@@ -245,11 +245,11 @@ defmodule ExAws.S3.Impl do
     request(client, :get, bucket, object)
   end
 
+  def put_object(client, bucket, object, body, opts, acl), do:
+    put_object(client, bucket, object, bucket, Keyword.put(opts, :'x-amz-acl', to_string acl))
+
   def put_object(client, bucket, object, body, opts \\ []) do
-    headers = [
-      {"Content-Type", "binary/octet-stream"} |
-      opts |> Map.to_list
-    ]
+    headers = Keyword.put_new(opts, :'content-type', "binary/octet-stream")
     request(client, :put, bucket, object, body: body, headers: headers)
   end
 
