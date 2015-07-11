3cd704537f5822110a1663fa49cd9d350b5b11b2
Ben Wilson <benwilson512@gmail.com>
2015-07-11 12:35:21 -0400

    slightly more general test

--- test/lib/ex_aws/auth_test.exs
+++ test/lib/ex_aws/auth_test.exs
@@ -5,7 +5,7 @@ defmodule ExAws.AuthTest do
   ]
 
   test "build_canonical_request can handle : " do
-    expected = "GET\n/bar%3Abaz\n\n\n\n\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    assert build_canonical_request("get", "http://foo.com/bar:baz", %{}, "") == expected
+    expected = "GET\n/bar%3Abaz%40blag\n\n\n\n\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    assert build_canonical_request("get", "http://foo.com/bar:baz@blag", %{}, "") == expected
   end
 end
