cf3a0417decc5ed1ec34985be61bef517d52cdaf
Ben Wilson <benwilson512@gmail.com>
2015-08-25 16:48:52 -0400

    vanity bump for docs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExAws.Mixfile do
 
   def project do
     [app: :ex_aws,
-     version: "0.4.6",
+     version: "0.4.7",
      elixir: "~> 1.0",
      description: "AWS client. Currently supports Dynamo, Kinesis, Lambda, S3",
      name: "ExAws",
