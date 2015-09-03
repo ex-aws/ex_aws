40bf9cd24e27fcd76f21e3394b241f730ec74967
Ben Wilson <benwilson512@gmail.com>
2015-09-03 11:37:28 -0400

    follow best practices about encoding structs, and enable structs to be nested nicely

--- lib/ex_aws/dynamo/encodable.ex
+++ lib/ex_aws/dynamo/encodable.ex
@@ -3,66 +3,99 @@ defprotocol ExAws.Dynamo.Encodable do
   @type t :: any
 
   @doc "Converts an elixir value into a map tagging the value with its dynamodb type"
-  def encode(value)
+  def encode(value, options)
 end
 
 defimpl ExAws.Dynamo.Encodable, for: Atom do
-  def encode(true),  do: %{"BOOL" => "true"}
-  def encode(false), do: %{"BOOL" => "false"}
-  def encode(value), do: %{"S" => value |> Atom.to_string}
+  def encode(true, _),  do: %{"BOOL" => "true"}
+  def encode(false, _), do: %{"BOOL" => "false"}
+  def encode(value, _), do: %{"S" => value |> Atom.to_string}
 end
 
 defimpl ExAws.Dynamo.Encodable, for: Integer do
-  def encode(val) do
+  def encode(val, _) do
     %{"N" => val |> Integer.to_string}
   end
 end
 
 defimpl ExAws.Dynamo.Encodable, for: Float do
-  def encode(val) do
+  def encode(val, _) do
     %{"N" => val |> Float.to_string}
   end
 end
 
 defimpl ExAws.Dynamo.Encodable, for: HashDict do
-  def encode(hashdict) do
+  def encode(hashdict, _) do
     %{"M" => ExAws.Dynamo.Encodable.Map.do_encode(hashdict)}
   end
 end
 
-defimpl ExAws.Dynamo.Encodable, for: Map do
-  def encode(%{__struct__: _} = struct) do
-    struct
-    |> Map.from_struct
-    |> do_encode
-  end
-  def encode(map) do
-    %{"M" => do_encode(map)}
+
+defimpl ExAws.Dynamo.Encodable, for: Any do
+  defmacro __deriving__(module, struct, options) do
+    deriving(module, struct, options)
   end
 
-  def do_encode(dict) do
-    Enum.reduce(dict, %{}, fn
+  def deriving(module, _struct, options) do
+    if only = options[:only] do
+      extractor = quote(do: Map.take(struct, unquote(only)))
+    else
+      extractor = quote(do: :maps.remove(:__struct__, struct))
+    end
+
+    quote do
+      defimpl ExAws.Dynamo.Encodable, for: unquote(module) do
+        def encode(struct, options) do
+          ExAws.Dynamo.Encodable.Map.encode(unquote(extractor), options)
+        end
+      end
+    end
+  end
+end
+
+defimpl ExAws.Dynamo.Encodable, for: Map do
+
+  defmacro __deriving__(module, struct, options) do
+    ExAws.Dynamo.Encodable.Any.deriving(module, struct, options)
+  end
+
+  def encode(map, options) do
+    %{"M" => do_encode(map, options)}
+  end
+
+  def do_encode(map, [only: only]) do
+    map
+    |> Map.take(only)
+    |> do_encode
+  end
+  def do_encode(map, [except: except]) do
+    :maps.without(except, map)
+    |> do_encode
+  end
+  def do_encode(map, _), do: do_encode(map)
+  def do_encode(map) do
+    Enum.reduce(map, %{}, fn
       ({_, nil}, map) -> map
       ({_, []}, map)  -> map
 
       ({k, v}, map) when is_binary(k) ->
-        Map.put(map, k, ExAws.Dynamo.Encodable.encode(v))
+        Map.put(map, k, ExAws.Dynamo.Encodable.encode(v, []))
       ({k, v}, map)   ->
         key = String.Chars.to_string(k)
-        Map.put(map, key, ExAws.Dynamo.Encodable.encode(v))
+        Map.put(map, key, ExAws.Dynamo.Encodable.encode(v, []))
     end)
   end
 end
 
 defimpl ExAws.Dynamo.Encodable, for: BitString do
-  def encode(val) do
+  def encode(val, _) do
     %{"S" => val}
   end
 end
 
 defimpl ExAws.Dynamo.Encodable, for: List do
   alias ExAws.Dynamo.Encodable
-  def encode([]), do: []
+  def encode([], _), do: []
 
   @doc """
   Dynamodb offers typed sets and L, a generic list of typed attributes.
@@ -74,13 +107,13 @@ defimpl ExAws.Dynamo.Encodable, for: List do
   Otherwise, it will use the generic list type ie
   [%{"N" => 1},%{"S" => "Foo"}] #=> %{"L" => [%{"N" => 1},%{"S" => "Foo"}]}
   """
-  def encode(list) do
+  def encode(list, _) do
     typed_values = list
-    |> Enum.map(&Encodable.encode/1)
+    |> Enum.map(&Encodable.encode(&1, []))
 
     {types, values} = Enum.reduce(list, {[], []}, fn(item, {types, values}) ->
       [{type, value}] = item
-      |> Encodable.encode
+      |> Encodable.encode([])
       |> Map.to_list
 
       {[type | types], [value | values]}
