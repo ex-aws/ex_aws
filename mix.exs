63f25a8960e1f70c79f8b1bf303043f24125c769
Ben Wilson <benwilson512@gmail.com>
2016-02-21 07:35:06 -0500

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExAws.Mixfile do
 
   def project do
     [app: :ex_aws,
-     version: "0.4.16",
+     version: "0.4.17",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      description: "AWS client. Currently supports Dynamo, Kinesis, Lambda, S3, SQS",
