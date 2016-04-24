271ad30f02ca49122a98ddaa2e0f332fbf1d6293
Sasha Fonseca <sashaafm@gmail.com>
2016-04-24 16:15:17 +0100

    Added the DescribeVpcAttribute action.

--- lib/ex_aws/ec2/client.ex
+++ lib/ex_aws/ec2/client.ex
@@ -265,6 +265,12 @@ defmodule ExAws.EC2.Client do
   defcallback delete_vpc(vpc_id :: binary) :: ExAws.Request.response_t
   defcallback delete_vpc(vpc_id :: binary, opts :: Map.t) :: ExAws.Request.response_t
 
+  @doc """
+  Describes the specified attribute of the specified VPC. You can specify only one attribute at a time.
+  """
+  defcallback describe_vpc_attribute(vpc_id :: binary, attribute :: binary) :: ExAws.Request.response_t
+  defcallback describe_vpc_attribute(vpc_id :: binary, attribute :: binary, opts :: Map.t) :: ExAws.Request.response_t
+
   defmacro __using__(opts) do 
 
     boilerplate = __MODULE__
