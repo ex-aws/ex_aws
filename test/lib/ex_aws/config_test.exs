389e148202878183ffcff5920e055917f9db246a
David <nogates@users.noreply.github.com>
2016-04-26 15:49:17 +0200

    Allow security token config (#141)
    * Allow security token as a common config field
    
    * Includes default value for security_token

--- test/lib/ex_aws/config_test.exs
+++ test/lib/ex_aws/config_test.exs
@@ -9,4 +9,17 @@ defmodule ExAws.ConfigTest do
     |> Map.get(:config)
     |> Map.get(:access_key_id) == value
   end
+
+  test "security_token is configured properly" do
+    value = "security_token"
+    System.put_env("AWS_SECURITY_TOKEN", value)
+    assert %{
+      __struct__: %{ config_root: [access_key_id: {:system, "AWS_SECURITY_TOKEN"}]},
+      config: %{},
+      service: :foo
+    }
+    |> ExAws.Config.build
+    |> Map.get(:config)
+    |> Map.get(:security_token) == value
+  end
 end
