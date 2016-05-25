31ec37bbbd5e802de54dd919c655a85d2b35b4ae
Sasha Fonseca <sashaafm@gmail.com>
2016-05-25 15:59:17 +0100

    Fix.

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule ExAws.Mixfile do
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.7", only: :dev},
       {:httpoison, "~> 0.7", optional: true},
-      {:poison, "~> 1.2 or 2.0", optional: true},
+      {:poison, "~> 1.2 or ~> 2.0", optional: true},
       {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2", optional: true},
       {:httpotion, "~> 2.0", optional: true},
       {:jsx, "~> 2.5", optional: true}
