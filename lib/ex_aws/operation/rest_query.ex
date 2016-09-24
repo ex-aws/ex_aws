b25faee5e38d5fa35f727d6539a75e3c3d390bc4
Akihide Kang <kanmo.ak@gmail.com>
2016-09-24 09:23:39 +0900

    Implement ExAws.Operation, for ExAws.Operation.RestQuery (#216)
    * implement ExAws.Operation, for ExAws.Operation.RestQuery
    
    * add EC2 Integration test for api that uses a rest query.

--- lib/ex_aws/operation/rest_query.ex
+++ lib/ex_aws/operation/rest_query.ex
@@ -5,4 +5,31 @@ defmodule ExAws.Operation.RestQuery do
     params: %{},
     service: nil,
   ]
+
+  @type t :: %__MODULE__{}
+end
+
+defimpl ExAws.Operation, for: ExAws.Operation.RestQuery do
+  def perform(operation, config) do
+    query = operation.params
+    |> URI.encode_query
+
+    headers = [
+      {"content-type", "application/x-www-form-urlencoded"},
+    ]
+
+    ExAws.Request.request(operation.http_method, url(config, operation.path), query, headers, config, operation.service)
+  end
+
+  def url(%{scheme: scheme, host: host, port: port}, queue_name) do
+    [
+      scheme,
+      host,
+      port |> port(),
+      queue_name
+    ] |> IO.iodata_to_binary
+  end
+
+  defp port(80), do: ""
+  defp port(p),  do: ":#{p}"
 end
