17969d2fb675e265f64f7eb7679de4ec9f27680d
Ben Wilson <benwilson512@gmail.com>
2016-10-01 11:15:45 -0400

    test updates

--- test/lib/ex_aws/sqs/integration_test.exs
+++ test/lib/ex_aws/sqs/integration_test.exs
@@ -2,7 +2,6 @@ defmodule ExAws.SQSIntegrationTest do
   use ExUnit.Case, async: true
 
   test "list_queues works" do
-    assert {:ok, %{body: body}} = ExAws.SQS.list_queues |> ExAws.request
-    assert body |> String.contains?("ListQueuesResponse")
+    assert {:ok, %{body: %{queues: _}}} = ExAws.SQS.list_queues |> ExAws.request
   end
 end
