e45b2c27336d5ebf57909cff128d39a5510d3e29
hirocaster <hohtsuka@gmail.com>
2016-10-21 16:36:29 +0900

    Add test encrypt and decrypt

--- test/lib/ex_aws/kms_test.exs
+++ test/lib/ex_aws/kms_test.exs
@@ -7,16 +7,23 @@ defmodule ExAws.KMSTest do
 
   test "#generate_data_key and #decrypt" do
     key_arn = System.get_env("AWS_KEY_ARN")
+
     assert {:ok, %{"CiphertextBlob" => ciphertext,
                    "KeyId" => key_id,
                    "Plaintext" => plaintext}} = ExAws.KMS.generate_data_key(key_arn) |> ExAws.request
 
     assert key_arn == key_id
 
+    {:ok, key} =  Base.decode64(plaintext)
+    iv = :crypto.strong_rand_bytes(32)
+    {encrypt_ciphertext, encrypt_ciphertag} = :crypto.block_encrypt(:aes_gcm, key, iv, { "", "hello"})
+
     assert {:ok, %{"KeyId" => decrypt_key_id,
                    "Plaintext" => decrypt_plaintext}} = ExAws.KMS.decrypt(ciphertext) |> ExAws.request
 
     assert key_id == decrypt_key_id
     assert plaintext == decrypt_plaintext
+
+    assert "hello" == :crypto.block_decrypt(:aes_gcm, key, iv, {"", encrypt_ciphertext, encrypt_ciphertag})
   end
 end
