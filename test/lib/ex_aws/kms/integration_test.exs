43d5c1bb5141df8060cd53a47b6acffeb50da2dc
hirocaster <hohtsuka@gmail.com>
2016-10-31 17:30:13 +0900

    Use only one key to Kms.re_encrypt integrate test

--- test/lib/ex_aws/kms/integration_test.exs
+++ test/lib/ex_aws/kms/integration_test.exs
@@ -90,22 +90,18 @@ defmodule ExAws.KMSIntegratinTest do
       assert {:ok, %{}} = ExAws.KMS.update_key_description(description, key_id) |> ExAws.request
     end
 
-    if System.get_env("AWS_KEY_ARN_2") do
-      test "ReEncrypt" do
-        assert {:ok, %{"CiphertextBlob" => ciphertext,
-                       "KeyId"          => key_id,
-                       "Plaintext"      => _plaintext}} = ExAws.KMS.generate_data_key(key_id) |> ExAws.request
+    test "ReEncrypt" do
+      assert {:ok, %{"CiphertextBlob" => ciphertext,
+                     "KeyId"          => encrypted_key_id,
+                     "Plaintext"      => _plaintext}} = ExAws.KMS.generate_data_key(key_id) |> ExAws.request
 
-        new_key_id = System.get_env("AWS_KEY_ARN_2")
+      assert {:ok, %{"CiphertextBlob" => re_encrypted_ciphertext,
+                     "KeyId"          => re_encrypted_key_id,
+                     "SourceKeyId"    => old_key_id}} = ExAws.KMS.re_encrypt(ciphertext, key_id) |> ExAws.request
 
-        assert {:ok, %{"CiphertextBlob" => re_encrypted_ciphertext,
-                       "KeyId"          => re_encrypted_key_id,
-                       "SourceKeyId"    => old_key_id}} = ExAws.KMS.re_encrypt(ciphertext, new_key_id) |> ExAws.request
-
-        assert key_id == old_key_id
-        assert ciphertext != re_encrypted_ciphertext
-        assert key_id != re_encrypted_key_id
-      end
+      assert ciphertext != re_encrypted_ciphertext
+      assert encrypted_key_id == re_encrypted_key_id
+      assert old_key_id == key_id
     end
   end
 end
