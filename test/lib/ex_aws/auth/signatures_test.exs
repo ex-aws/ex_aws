2c0b5838628ebe4025ca665fe4f19e519349e53d
Danyil Velichko <danvelduck@gmail.com>
2016-11-08 12:05:13 +0700

    Add signature test

--- test/lib/ex_aws/auth/signatures_test.exs
+++ test/lib/ex_aws/auth/signatures_test.exs
@@ -16,4 +16,19 @@ defmodule ExAws.Auth.SignaturesTest do
       assert signature == "690b8431208dae486dd00df93bde9370d8aba587098b9a26bfd07c259df395c9"
     end
   end
+
+  describe "generate_signature_v2/4" do
+    test "with a basic string to sign" do
+      config = ExAws.Config.new(:s3, [
+        access_key_id: "AKIAIOSFODNN7EXAMPLE",
+        secret_access_key: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+        region: "us-east-1"
+      ])
+      datetime = {{2016,8,29},{19,41,33}}
+
+      signature = Signatures.generate_signature_v2("s3", config, datetime, "hello world")
+
+      assert signature == "f8TffUgcy9sMw70uXsnqdim8aiw="
+    end
+  end
 end
