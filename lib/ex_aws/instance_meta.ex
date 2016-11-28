ab6b105ca0b4a213755d1afb04dbc823168e2445
hamstah <hamstahguru@gmail.com>
2016-11-28 14:37:38 +0000

    Added support for ECS task role credentials

--- lib/ex_aws/instance_meta.ex
+++ lib/ex_aws/instance_meta.ex
@@ -8,19 +8,38 @@ defmodule ExAws.InstanceMeta do
   # AWS InstanceMetaData URL
   @meta_path_root "http://169.254.169.254/latest/meta-data/"
 
-  def request(config, path) do
-    case config.http_client.request(:get, @meta_path_root <> path) do
+  # Endpoint for ECS taks role credentials
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-iam-roles.html
+  @task_role_root "http://169.254.170.2"
+
+  def request(config, url) do
+    case config.http_client.request(:get, url) do
       {:ok, %{body: body}} -> body
     end
   end
 
-  def role(config) do
+  def instance_role(config) do
     ExAws.InstanceMeta.request(config, "/iam/security-credentials/")
   end
 
-  def security_credentials(config) do
-    result = ExAws.InstanceMeta.request(config, "/iam/security-credentials/#{role(config)}")
+  def task_role_credentials(config) do
+    case System.get_env("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI") do
+      nil -> nil
+      uri -> ExAws.InstanceMeta.request(config, @task_role_root <> uri)
+             |> config.json_codec.decode!
+    end
+  end
+
+  def instance_role_credentials(config) do
+    ExAws.InstanceMeta.request(config, @meta_path_root <> "/iam/security-credentials/#{instance_role(config)}")
     |> config.json_codec.decode!
+  end
+
+  def security_credentials(config) do
+    result = case task_role_credentials(config) do
+	       nil -> instance_role_credentials(config)
+	       credentials -> credentials
+	     end
 
     %{
       access_key_id: result["AccessKeyId"],
