77590bcdd717ebeb5000a190edb2e173fbf136a3
kanmo <kanmo.ak@gmail.com>
2016-12-04 17:24:55 +0900

    fix Dynamo.Decorder.decode when parameter is map

--- lib/ex_aws/dynamo/decoder.ex
+++ lib/ex_aws/dynamo/decoder.ex
@@ -41,8 +41,8 @@ defmodule ExAws.Dynamo.Decoder do
   end
   def decode(%{"N" => value}) when is_binary(value), do: binary_to_number(value)
   def decode(%{"N" => value}) when value |> is_integer or value |> is_float, do: value
-  def decode(item = %{}) do
-    item |> Enum.reduce(%{}, fn({k, v}, map) ->
+  def decode(%{"Item" => value}) do
+    value |> Enum.reduce(%{}, fn({k,v}, map) ->
       Map.put(map, k, decode(v))
     end)
   end
