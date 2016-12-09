cd3eb61ce0bc71bef77739e8ae62c6b9d1e68b66
Boris Mikhaylov <kaguxmail@gmail.com>
2016-12-09 09:32:47 +0100

    clean up url in request url module

--- lib/ex_aws/request/url.ex
+++ lib/ex_aws/request/url.ex
@@ -5,19 +5,28 @@ defmodule ExAws.Request.Url do
   Builds URL for an operation and a config"
   """
   def build(operation, config) do
-    query =  operation.params |> URI.encode_query
+    query =  operation.params |> normalize_params |> URI.encode_query
     config
     |> Map.take([:scheme, :host, :port])
-    |> normalize_scheme
     |> Map.put(:query, query)
     |> Map.put(:path, operation.path)
+    |> normalize_scheme
+    |> normalize_path
     |> (&struct(URI, &1)).()
     |> URI.to_string
+    |> String.trim_trailing("?")
   end 
 
-  defp normalize_scheme(%{scheme: scheme} = url) do
-    scheme = scheme |> String.replace("://", "")
-    url |> Map.put(:scheme, scheme)
+  defp normalize_scheme(url) do
+    url |> Map.update(:scheme, "", &String.replace(&1, "://", ""))
   end
-  defp normalize_scheme(url), do: url
+
+  defp normalize_path(url) do
+    url |> Map.update(:path, "", &String.replace(&1, ~r/\/{2,}/, "/"))
+  end
+
+  defp normalize_params(params) when is_map(params)  do
+    params |> Map.delete("") |> Map.delete(nil)
+  end
+  defp normalize_params(params), do: params
 end
--- test/lib/ex_aws/request/url_test.exs
+++ test/lib/ex_aws/request/url_test.exs
@@ -1,10 +1,9 @@
 defmodule ExAws.Request.UrlTest do
   use ExUnit.Case, async: true
   alias ExAws.Request.Url
-  alias ExAws.Operation.RestQuery
 
   setup do
-    rest_query = %RestQuery{
+    query = %{
       path: "/path",
       params: %{foo: :bar},
     }
@@ -13,20 +12,50 @@ defmodule ExAws.Request.UrlTest do
       host: "example.com",
       port: 443
     }
-    {:ok, %{rest_query: rest_query, config: config}}
+    {:ok, %{query: query, config: config}}
   end
 
-  test "it build urls for rest query operation", %{rest_query: rest_query, config: config} do
-    assert Url.build(rest_query, config) == "https://example.com/path?foo=bar"
+  test "it build urls for query operation", %{query: query, config: config} do
+    assert Url.build(query, config) == "https://example.com/path?foo=bar"
   end
 
-  test "it allows setting custom port", %{rest_query: rest_query, config: config} do
+  test "it allows setting custom port", %{query: query, config: config} do
     config = config |> Map.put(:port, 4430)
-    assert Url.build(rest_query, config) == "https://example.com:4430/path?foo=bar"
+    assert Url.build(query, config) == "https://example.com:4430/path?foo=bar"
   end
 
-  test "it allows passing scheme with trailing ://", %{rest_query: rest_query, config: config} do
+  test "it allows passing scheme with trailing ://", %{query: query, config: config} do
     config = config |> Map.put(:scheme, "https://")
-    assert Url.build(rest_query, config) == "https://example.com/path?foo=bar"
+    assert Url.build(query, config) == "https://example.com/path?foo=bar"
+  end
+
+  test "it accepts params as a list of keywords", %{query: query, config: config} do
+    query = query |> Map.put(:params, [foo: :bar])
+    assert Url.build(query, config) == "https://example.com/path?foo=bar"
+  end
+
+  test "it does not have trailing ? when params is an empty map", %{query: query, config: config} do
+    query = query |> Map.put(:params, %{})
+    assert Url.build(query, config) == "https://example.com/path"
+  end
+
+  test "it does not have trailing ? when params is an empty list", %{query: query, config: config} do
+    query = query |> Map.put(:params, [])
+    assert Url.build(query, config) == "https://example.com/path"
+  end
+
+  test "it cleans up excessive slashes in the path", %{query: query, config: config} do
+    query = query |> Map.put(:path, "//path///with/too/many//slashes//")
+    assert Url.build(query, config) == "https://example.com/path/with/too/many/slashes/?foo=bar"
+  end
+
+  test "it ignores empty parameter key", %{query: query, config: config} do
+    query = query |> Map.put(:params, %{"foo" => "bar", "" => 1})
+    assert Url.build(query, config) == "https://example.com/path?foo=bar"
+  end
+
+  test "it ignores nil parameter key", %{query: query, config: config} do
+    query = query |> Map.put(:params, %{"foo" => "bar", nil => 1})
+    assert Url.build(query, config) == "https://example.com/path?foo=bar"
   end
 end
