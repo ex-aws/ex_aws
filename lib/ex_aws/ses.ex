a63729bbe75c186dc6d594b3ad77ea0920e728ef
Yevhenii Kurtov <lessless@users.noreply.github.com>
2017-01-09 22:13:03 +0700

    Add SES service support (#304)
    * Add SES.verify_email_identity query
    
    * Add SES.identity_verification_attributes query
    
    * Add error parsing for SES service
    
    * Set default SES region to us-east-1
    
    * Add SES.configuration_sets query
    
    * Add SES.SendEmail query
    
    * Add #verify_email_identity request

---
+++ lib/ex_aws/ses.ex
@@ -0,0 +1,140 @@
+defmodule ExAws.SES do
+  import ExAws.Utils, only: [camelize_key: 1, camelize_keys: 1]
+
+  @moduledoc """
+  Operations on AWS SES
+
+  http://docs.aws.amazon.com/ses/latest/APIReference/Welcome.html
+  """
+
+  @doc "Verifies an email address"
+  @spec verify_email_identity(email :: binary) :: ExAws.Operation.Query.t
+  def verify_email_identity(email) do
+    request(:verify_email_identity, %{"EmailAddress" => email})
+  end
+
+  @doc "Fetch identities verification status and token (for domains)"
+  @spec identity_verification_attributes([binary]) :: ExAws.Operation.Query.t
+  def identity_verification_attributes(identities) when is_list(identities) do
+    params = format_member_attribute({:identities, identities})
+    request(:get_identity_verification_attributes, params)
+  end
+
+  @doc "Fetch configuration sets associated with AWS account"
+  @spec configuration_sets(list) :: ExAws.Operation.t
+  def configuration_sets(opts \\ []) do
+    params = build_opts(opts, [:max_items, :next_token])
+    request(:list_configuration_sets, params)
+  end
+
+
+  ## Emails
+  ######################
+  @type message :: %{
+    body: %{html: %{data: binary, charset: binary}, text: %{data: binary, charset: binary}},
+    subject: %{data: binary, charset: binary}
+  }
+  @type destination :: %{cc_addresses: [binary], bcc_addresses: [binary], bcc_addresses: [binary]}
+
+  @doc "Composes an email message"
+  @type send_email(dst :: destination, msg :: message, src :: binary, opts ::list) :: ExAws.Operation.t
+  def send_email(dst, msg, src, opts \\ []) do
+    params = opts
+    |> build_opts([:configuration_set_name, :return_path, :return_path_arn, :source_arn, :bcc])
+    |> Map.merge(format_member_attribute(:reply_to_addresses, opts[:reply_to_addresses]))
+    |> Map.merge(flatten_attrs(msg, "message"))
+    |> Map.merge(format_tags(opts[:tags]))
+    |> Map.merge(format_dst(dst))
+    |> Map.put_new("Source", src)
+
+    request(:send_email, params)
+  end
+
+  @doc "Build message object"
+  @spec build_message(binary, binary, binary, binary) :: message
+  def build_message(html, txt, subject, charset \\ "UTF-8") do
+    %{
+      body: %{
+        html: %{data: html, charset: charset},
+        text: %{data: txt, charset: charset}
+      },
+      subject: %{data: subject, charset: charset}
+    }
+  end
+
+  defp format_dst(dst) do
+     Map.to_list(dst)
+    |> format_member_attributes([:bcc_addresses, :cc_addresses, :to_addresses])
+    |> flatten_attrs("destination")
+  end
+
+  defp format_tags(nil), do: %{}
+
+  defp format_tags(tags) do
+    tags
+    |> Enum.with_index
+    |> Enum.reduce(%{}, fn({tag, index}, acc) ->
+      key = camelize_key("tags.member.#{index + 1}")
+      Map.merge(acc, flatten_attrs(tag, key))
+    end)
+  end
+
+
+  ## Request
+  ######################
+
+  defp request(action, params) do
+    action_string = action |> Atom.to_string |> Macro.camelize
+
+    %ExAws.Operation.Query{
+      path: "/",
+      params: params |> Map.put("Action", action_string),
+      service: :ses,
+      action: action,
+      parser: &ExAws.SES.Parsers.parse/2
+    }
+  end
+
+  defp build_opts(opts, permitted) do
+    opts
+    |> Map.new
+    |> Map.take(permitted)
+    |> camelize_keys
+  end
+
+  defp format_member_attributes(opts, members) do
+    opts
+    |> Map.new
+    |> Map.take(members)
+    |> Enum.reduce(Map.new(opts), fn(entry, acc) -> Map.merge(acc, format_member_attribute(entry)) end)
+    |> Map.drop(members)
+  end
+
+  defp format_member_attribute(key, collection), do: format_member_attribute({key, collection})
+
+  defp format_member_attribute({_, nil}), do: %{}
+
+  defp format_member_attribute({key, collection}) do
+    collection
+    |> Enum.with_index
+    |> Enum.reduce(%{}, fn({item, index}, params) ->
+      Map.put_new(params, "#{camelize_key(key)}.member.#{index + 1}", item)
+    end)
+  end
+
+  defp flatten_attrs(attrs, root) do
+    do_flatten_attrs({attrs, camelize_key(root)})
+    |> List.flatten
+    |> Map.new
+  end
+
+  defp do_flatten_attrs({attrs, root}) when is_map(attrs) do
+    Enum.map(attrs, fn ({k, v}) ->
+      do_flatten_attrs({v, root <> "." <> camelize_key(k)})
+    end)
+  end
+
+  defp do_flatten_attrs({val, path}) do
+    {camelize_key(path), val}
+  end
+end
---
+++ test/lib/ex_aws/ses/parser_test.exs
@@ -0,0 +1,141 @@
+
+defmodule ExAws.SNS.ParserTest do
+  use ExUnit.Case, async: true
+
+  alias ExAws.SES.Parsers
+
+  def to_success(doc) do
+    {:ok, %{body: doc}}
+  end
+
+  def to_error(doc) do
+    {:error, {:http_error, 403, %{body: doc}}}
+  end
+
+  test "#parse a verify_email_identity response" do
+    rsp = """
+      <VerifyEmailIdentityResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+      <VerifyEmailIdentityResult/>
+        <ResponseMetadata>
+          <RequestId>d8eb8250-be9b-11e6-b7f7-d570946af758</RequestId>
+        </ResponseMetadata>
+      </VerifyEmailIdentityResponse>
+    """
+    |> to_success
+
+
+    {:ok, %{body: parsed_doc}} = Parsers.parse(rsp, :verify_email_identity)
+    assert parsed_doc == %{request_id: "d8eb8250-be9b-11e6-b7f7-d570946af758"}
+  end
+
+  test "#parse identity_verification_attributes" do
+    rsp = """
+      <GetIdentityVerificationAttributesResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+        <GetIdentityVerificationAttributesResult>
+          <VerificationAttributes>
+            <entry>
+              <key>example.com</key>
+              <value>
+                <VerificationToken>pwCRTZ8zHIJu+vePnXEa4DJmDyGhjSS8V3TkzzL2jI8=</VerificationToken>
+                <VerificationStatus>Pending</VerificationStatus>
+              </value>
+            </entry>
+            <entry>
+              <key>user@example.com</key>
+              <value>
+                <VerificationStatus>Pending</VerificationStatus>
+              </value>
+            </entry>
+          </VerificationAttributes>
+        </GetIdentityVerificationAttributesResult>
+        <ResponseMetadata>
+          <RequestId>f5e3ef21-bec1-11e6-b618-27019a58dab9</RequestId>
+        </ResponseMetadata>
+      </GetIdentityVerificationAttributesResponse>
+    """
+    |> to_success
+
+    verification_attributes = %{
+      "example.com" => %{
+        verification_token: "pwCRTZ8zHIJu+vePnXEa4DJmDyGhjSS8V3TkzzL2jI8=",
+        verification_status: "Pending"
+      },
+      "user@example.com" => %{
+        verification_status: "Pending"
+      }
+    }
+
+
+    {:ok, %{body: parsed_doc}} = Parsers.parse(rsp, :get_identity_verification_attributes)
+    assert parsed_doc[:verification_attributes] == verification_attributes
+  end
+
+  test "#parse configuration_sets" do
+    rsp = """
+      <ListConfigurationSetsResponse xmlns=\"http://ses.amazonaws.com/doc/2010-12-01/\">
+        <ListConfigurationSetsResult>
+          <ConfigurationSets>
+            <member>
+              <Name>test</Name>
+            </member>
+          </ConfigurationSets>
+          <NextToken>QUFBQUF</NextToken>
+        </ListConfigurationSetsResult>
+        <ResponseMetadata>
+          <RequestId>c177d6ce-c1b0-11e6-9770-29713cf492ad</RequestId>
+        </ResponseMetadata>
+      </ListConfigurationSetsResponse>
+    """
+    |> to_success
+
+    configuration_sets = %{
+      members: ["test"],
+      next_token: "QUFBQUF"
+    }
+
+    {:ok, %{body: parsed_doc}} = Parsers.parse(rsp, :list_configuration_sets)
+    assert parsed_doc[:configuration_sets] == configuration_sets
+  end
+
+  test "#parse send_email" do
+    rsp = """
+    <SendEmailResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+      <SendEmailResult>
+        <MessageId>0100015914b22075-7a4e3573-ca72-41ce-8eda-388f81232ad9-000000</MessageId>
+      </SendEmailResult>
+      <ResponseMetadata>
+        <RequestId>8194094b-c58a-11e6-b49d-838795cc7d3f</RequestId>
+      </ResponseMetadata>
+    </SendEmailResponse>
+    """
+    |> to_success
+
+    {:ok, %{body: parsed_doc}} = Parsers.parse(rsp, :send_email)
+    assert parsed_doc == %{
+      request_id: "8194094b-c58a-11e6-b49d-838795cc7d3f",
+      message_id: "0100015914b22075-7a4e3573-ca72-41ce-8eda-388f81232ad9-000000"
+    }
+  end
+
+  test "#parse error" do
+    rsp = """
+      <ErrorResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/\">
+        <Error>
+          <Type>Sender</Type>
+          <Code>MalformedInput</Code>
+          <Message>Top level element may not be treated as a list</Message>
+        </Error>
+        <RequestId>3ac0a9e8-bebd-11e6-9ec4-e5c47e708fa8</RequestId>
+      </ErrorResponse>
+    """
+    |> to_error
+
+
+    {:error, {:http_error, 403, err}} = Parsers.parse(rsp, :get_identity_verification_attributes)
+
+    assert "Sender" == err[:type]
+    assert "MalformedInput" == err[:code]
+    assert "Top level element may not be treated as a list" == err[:message]
+    assert "3ac0a9e8-bebd-11e6-9ec4-e5c47e708fa8" == err[:request_id]
+  end
+end
