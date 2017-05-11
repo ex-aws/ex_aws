9cb772233d45e9d0c4238932ac2d233fbdee81c8
Andre Van Der Merwe <andre@andrevdm.com>
2017-05-11 15:59:25 +0200

    Add tests for cloudformation's describe_stacks

--- test/lib/ex_aws/cloudformation/integration_test.exs
+++ test/lib/ex_aws/cloudformation/integration_test.exs
@@ -4,4 +4,8 @@ defmodule ExAws.CloudformationIntegrationTest do
   test "list_stacks works" do
     assert {:ok, %{body: %{stacks: _}}} = ExAws.Cloudformation.list_stacks(status_filter: [:update_complete, :create_complete]) |> ExAws.request
   end
+
+  test "describe_stacks works" do
+    assert {:ok, %{body: %{stacks: _}}} = ExAws.Cloudformation.describe_stacks(nil) |> ExAws.request
+  end
 end
