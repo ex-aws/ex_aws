e75fae235c81e8db18e281145a66dd6af89362fd
Kyle A Anderson <kyleaa@gmail.com>
2017-06-28 16:48:40 -0400

    add sts methods: assume role, ...
    decode authorization response,
    get session token

--- lib/ex_aws/sts/parsers.ex
+++ lib/ex_aws/sts/parsers.ex
@@ -2,6 +2,30 @@ if Code.ensure_loaded?(SweetXml) do
   defmodule ExAws.STS.Parsers do
     import SweetXml, only: [sigil_x: 2]
 
+    def parse({:ok, %{body: xml}=resp}, :assume_role) do
+      parsed_body = xml
+      |> SweetXml.xpath(~x"//AssumeRoleResponse",
+                        access_key_id: ~x"./AssumeRoleResult/Credentials/AccessKeyId/text()"s,
+                        secret_access_key: ~x"./AssumeRoleResult/Credentials/SecretAccessKey/text()"s,
+                        session_token: ~x"./AssumeRoleResult/Credentials/SessionToken/text()"s,
+                        expiration: ~x"./AssumeRoleResult/Credentials/Expiration/text()"s,
+                        assumed_role_id: ~x"./AssumeRoleResult/AssumedRoleUser/AssumedRoleId/text()"s,
+                        assumed_role_arn: ~x"./AssumeRoleResult/AssumedRoleUser/Arn/text()"s,
+                        request_id: request_id_xpath())
+
+      {:ok, Map.put(resp, :body, parsed_body)}
+    end
+
+    def parse({:ok, %{body: xml}=resp}, :decode_authorization_message) do
+      parsed_body = xml
+      |> SweetXml.xpath(~x"//DecodeAuthorizationMessageResponse",
+                        decoded_message: ~x"./DecodeAuthorizationMessageResult/DecodedMessage/text()"s,
+                        request_id: request_id_xpath())
+      |> Map.update!(:decoded_message, &Poison.decode!/1)
+
+      {:ok, Map.put(resp, :body, parsed_body)}
+    end
+
     def parse({:ok, %{body: xml} = resp}, :get_caller_identity) do
       parsed_body = SweetXml.xpath(xml, ~x"//GetCallerIdentityResponse", [
         arn: ~x"./GetCallerIdentityResult/Arn/text()"s,
@@ -20,6 +44,18 @@ if Code.ensure_loaded?(SweetXml) do
                         secret_access_key: ~x"./GetFederationTokenResult/Credentials/SecretAccessKey/text()"s,
                         session_token: ~x"./GetFederationTokenResult/Credentials/SessionToken/text()"s,
                         expiration: ~x"./GetFederationTokenResult/Credentials/Expiration/text()"s,
+                        request_id: request_id_xpath())
+
+      {:ok, Map.put(resp, :body, parsed_body)}
+    end
+
+    def parse({:ok, %{body: xml}=resp}, :get_session_token) do
+      parsed_body = xml
+      |> SweetXml.xpath(~x"//GetSessionTokenResponse",
+                        access_key_id: ~x"./GetSessionTokenResult/Credentials/AccessKeyId/text()"s,
+                        secret_access_key: ~x"./GetSessionTokenResult/Credentials/SecretAccessKey/text()"s,
+                        session_token: ~x"./GetSessionTokenResult/Credentials/SessionToken/text()"s,
+                        expiration: ~x"./GetSessionTokenResult/Credentials/Expiration/text()"s,
                         request_id: request_id_xpath())
 
       {:ok, Map.put(resp, :body, parsed_body)}
