c27b8ab5a69894180249d490e936ea0cfc2e091f
Ben Johnson <bjohnson@binarylogic.com>
2017-06-30 10:04:45 -0500

    Add stream support for Dynamo.query/2 (#422)
    * Add stream support for queries
    
    * User proper method name

--- lib/ex_aws/dynamo.ex
+++ lib/ex_aws/dynamo.ex
@@ -325,7 +325,7 @@ defmodule ExAws.Dynamo do
     |> build_opts
     |> Map.merge(%{"TableName" => name})
 
-    request(:query, data)
+    request(:query, data, %{stream_builder: &ExAws.Dynamo.Lazy.stream_query(name, opts, &1)})
   end
 
   @doc """
