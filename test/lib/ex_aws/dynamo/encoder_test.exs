19df2f953c24299c8f034f901a2e7294ce3fd336
Masanori Tanaka <m-tanaka@eole.co.jp>
2017-07-02 03:21:20 +0900

    Modify ExAws.Dynamo.Encodable.encode for: List to handle encoding an … (#424)
    * Modify ExAws.Dynamo.Encodable.encode for: List to handle encoding an all number List to Dynamo's NS(Number Set) type.
    
    * Revert "Modify ExAws.Dynamo.Encodable.encode for: List to handle encoding an all number List to Dynamo's NS(Number Set) type."
    
    This reverts commit c1f6184be4b55e826facf154019107f5575dcce2.
    
    * Added ExAws.Dynamo.Encodable.encode for: MapSet
    
    This patch enables to encode an all number MapSet to Dynamo's NS(Number Set) type and encode an all string MapSet to Dynamo's SS(String Set) type.
    
    * Added ExAws.Dynamo.Encodable.encode for: MapSet
    
    This patch enables to encode an all number MapSet to Dynamo's NS(Number Set) type and encode an all string MapSet to Dynamo's SS(String Set) type.

--- test/lib/ex_aws/dynamo/encoder_test.exs
+++ test/lib/ex_aws/dynamo/encoder_test.exs
@@ -31,6 +31,17 @@ defmodule ExAws.Dynamo.EncoderTest do
     assert ["foo", 1] |> Encoder.encode == %{"L" => [%{"S" => "foo"}, %{"N" => "1"}]}
   end
 
+  test "encoder handles mapsets properly" do
+    assert MapSet.new([1, 2, 3]) |> Encoder.encode == %{"NS" => ["1", "2", "3"]}
+    assert MapSet.new(["A", "B", "C"]) |> Encoder.encode == %{"SS" => ["A", "B", "C"]}
+    assert_raise RuntimeError, "Cannot determine a proper data type for an empty MapSet", fn ->
+      MapSet.new([]) |> Encoder.encode
+    end
+    assert_raise RuntimeError, "All elements in a MapSet must be only numbers or only strings", fn ->
+      MapSet.new([1, "A"]) |> Encoder.encode
+    end
+  end
+
   test "encoder is idempotent" do
     value = %{foo: 1, bar: %{baz: 2, zounds: "asdf"}}
     assert value |> Encoder.encode == value |> Encoder.encode |> Encoder.encode
