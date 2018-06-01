f532d149af6dd54fe7d33a8082d7626d3432c8e8
Adam Rainey <adamrainey747@gmail.com>
2018-05-31 18:53:11 -0700

    Remove camelize keys and it's tests

--- test/ex_aws/utils_test.exs
+++ test/ex_aws/utils_test.exs
@@ -2,34 +2,6 @@ defmodule ExAws.UtilsTest do
   use ExUnit.Case, async: true
   import ExAws.Utils
 
-  test "camelize_keys works with maps" do
-    assert %{"HelloWorld" => "foo"} == %{hello_world: "foo"}
-    |> camelize_keys
-  end
-
-  test "camelize_keys does not go deep unless specified maps" do
-    assert %{"FooBar" => %{foo_bar: "baz"}} == %{foo_bar: %{foo_bar: "baz"}}
-    |> camelize_keys
-
-    assert %{"FooBar" => %{"FooBar" => "baz"}} == %{foo_bar: %{foo_bar: "baz"}}
-    |> camelize_keys(deep: true)
-  end
-
-  test "camelize_keys can handle keyword lists" do
-    assert %{"FooBar" => %{"FooBar" => "baz"}} == [foo_bar: [foo_bar: "baz"]]
-    |> camelize_keys(deep: true)
-  end
-
-  test "camelize_keys can handle handle lists that aren't keyword lists" do
-    assert %{"FooBar" => ["foo", "bar"]} == [foo_bar: ["foo", "bar"]]
-    |> camelize_keys(deep: true)
-  end
-
-  test "camelize_keys spec works for non-standard keys" do
-    assert %{"non-standard" => ["foo", "bar"]} == [foo_bar: ["foo", "bar"]]
-    |> camelize_keys(spec: %{foo_bar: "non-standard"})
-  end
-
   test "iso_z_to_secs converts iso string to epoch seconds" do
     assert 1436134578 == iso_z_to_secs("2015-07-05T22:16:18Z")
   end
@@ -57,10 +29,10 @@ defmodule ExAws.UtilsTest do
       {"Tag.2.Key", "keyB"}, {"Tag.2.Value", "keyB"},
       {"Member", "member!"}
     ]
-    
+
     assert expected_return == format([
-      tag: [ 
-        [key: "keyA", value: "ValueA"], 
+      tag: [
+        [key: "keyA", value: "ValueA"],
         [key: "keyB", value: "keyB"],
       ],
       member: "member!"
