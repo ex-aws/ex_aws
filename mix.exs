baf0cadde67c255febbfaf79f095f39978dabd04
Aleksei Magusev <lexmag@me.com>
2018-07-23 16:41:20 +0200

    Remove unnecessary function clause (#580)

--- mix.exs
+++ mix.exs
@@ -30,11 +30,7 @@ defmodule ExAws.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  defp deps do
-    deps(:test_dev)
-  end
-
-  defp deps(:test_dev) do
+  defp deps() do
     [
       {:sweet_xml, "~> 0.6", optional: true},
       {:ex_doc, "~> 0.16", only: [:dev, :test]},
