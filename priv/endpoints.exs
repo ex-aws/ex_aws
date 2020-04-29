1b584d221054deedf50df5ed627e2a801e03fb5f
Eric Saxby <sax@livinginthepast.org>
2020-04-29 12:13:02 -0700

    Add connect to endpoint default configuration (#689)

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -30,6 +30,17 @@
         "us-west-2" => %{"description" => "US West (Oregon)"}
       },
       "services" => %{
+        "connect" => %{
+          "endpoints" => %{
+            "ap-northeast-1" => %{},
+            "ap-southeast-1" => %{},
+            "ap-southeast-2" => %{},
+            "eu-central-1" => %{},
+            "eu-west-2" => %{},
+            "us-east-1" => %{},
+            "us-west-2" => %{}
+          }
+        },
         "firehose" => %{
           "endpoints" => %{
             "ap-northeast-1" => %{},
