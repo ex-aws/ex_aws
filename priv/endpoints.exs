42df92402896e90bece718063a8dd9c2df7afaef
tomohaya <tomo.skyblue218@gmail.com>
2020-10-23 14:38:55 +0900

    Add comprehend endpoint
    https://docs.aws.amazon.com/general/latest/gr/comprehend.html

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -1439,7 +1439,14 @@
             "eu-west-1" => %{},
             "us-east-1" => %{},
             "us-east-2" => %{},
-            "us-west-2" => %{}
+            "us-west-2" => %{},
+            "ap-south-1" => %{},
+            "ap-northeast-2" => %{},
+            "ap-southeast-1" => %{},
+            "ap-southeast-2" => %{},
+            "ap-northeast-1" => %{},
+            "ca-central-1" => %{},
+            "us-gov-west-1" => %{}
           }
         },
         "textract" => %{
