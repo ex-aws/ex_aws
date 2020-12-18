62ea485b1c93f293ac9508394c726ce88d74e7d7
Aiden Scandella <git@sca.ndella.com>
2020-12-18 12:10:59 -0800

    Add us-east-2 as allowable SES email endpoint

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -1549,6 +1549,7 @@
             "eu-west-1" => %{},
             "eu-west-2" => %{},
             "us-east-1" => %{},
+            "us-east-2" => %{},
             "us-west-2" => %{},
             "ca-central-1" => %{},
             "sa-east-1" => %{}
