51dd70611a398d1fc66716441471a90ec76985c0
ZombieHarvester <ZombieHarvester@users.noreply.github.com>
2021-09-04 13:45:00 +0300

    Restrict mime version to 1.2

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule ExAws.Mixfile do
   defp deps() do
     [
       {:telemetry, "~> 0.4.3 or ~> 1.0"},
+      {:mime, "~> 1.2"}, # mime 2.x requires Elixir ~> 1.10
       {:bypass, "~> 2.1", only: :test},
       {:configparser_ex, "~> 4.0", optional: true},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
