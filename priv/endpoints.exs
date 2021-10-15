1a865a47af538ee50afb5b7f6fa0c8ad383c27c5
Mark Butler <mark.butler@smartrent.com>
2021-10-15 13:34:09 -0700

    TECH Add supported endpoints for Rekognition (#1)
    * Added other supported endpoints to Rekognition.

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -1279,12 +1279,17 @@
         },
         "rekognition" => %{
           "endpoints" => %{
+            "ap-south-1" => %{},
             "ap-northeast-1" => %{},
+            "ap-northeast-2" => %{},
             "ap-southeast-1" => %{},
             "ap-southeast-2" => %{},
+            "ca-central-1" => %{},
+            "eu-central-1" => %{},
             "eu-west-1" => %{},
             "us-east-1" => %{},
             "us-east-2" => %{},
+            "us-west-1" => %{},
             "us-west-2" => %{}
           }
         },
