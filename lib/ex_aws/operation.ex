6fb9e35a6558cd7690218275d8c0efd6164969d2
Andrea Leopardi <an.leopardi@gmail.com>
2022-01-23 13:38:29 +0100

    Add a few specs and polish docs here and there

--- lib/ex_aws/operation.ex
+++ lib/ex_aws/operation.ex
@@ -1,9 +1,9 @@
 defprotocol ExAws.Operation do
-  @doc """
-  An operation to perform on AWS
+  @moduledoc """
+  An operation to perform on AWS.
 
   This module defines a protocol for executing operations on AWS. ExAws ships with
-  several different modules that each implement the ExAws.Operation protocol. These
+  several different modules that each implement the `ExAws.Operation` protocol. These
   modules each handle one of the broad categories of AWS service types:
 
   - `ExAws.Operation.JSON`
@@ -13,15 +13,22 @@ defprotocol ExAws.Operation do
 
   ExAws works by creating a data structure that implements this protocol, and then
   calling `perform/2` on it.
+  """
+
+  @doc """
+  Perform a request on AWS.
+
+  The operation is synchronous, returning a response or an error.
 
   ## Example
 
       %ExAws.Operation.JSON{
         data: %{},
         headers: [
-        {"x-amz-target", "DynamoDB_20120810.ListTables"},
+          {"x-amz-target", "DynamoDB_20120810.ListTables"},
           {"content-type", "application/x-amz-json-1.0"}
-        ], http_method: :post,
+        ],
+        http_method: :post,
         params: %{},
         path: "/",
         service: :dynamodb,
@@ -30,5 +37,8 @@ defprotocol ExAws.Operation do
   """
   def perform(operation, config)
 
+  @doc """
+  Perform a *streaming* request on AWS.
+  """
   def stream!(operation, config)
 end
