613d7b3208ce868c7d7f7cdcd2dee3a16e317d39
Jingjing Lin <Jingjinglin@tubi.tv>
2022-06-17 12:29:34 +0800

    add more supported endpoints for medialive

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -1397,14 +1397,20 @@
         },
         "medialive" => %{
           "endpoints" => %{
+            "ap-south-1" => %{},
             "ap-northeast-1" => %{},
             "ap-northeast-2" => %{},
             "ap-southeast-1" => %{},
             "ap-southeast-2" => %{},
             "eu-central-1" => %{},
             "eu-west-1" => %{},
+            "eu-west-2" => %{},
+            "eu-west-3" => %{},
+            "eu-north-1" => %{},
             "us-east-1" => %{},
-            "us-west-2" => %{}
+            "us-east-2" => %{},
+            "us-west-2" => %{},
+            "sa-east-1" => %{}
           }
         },
         "discovery" => %{"endpoints" => %{"us-west-2" => %{}}},
