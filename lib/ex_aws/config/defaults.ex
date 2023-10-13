f3a47aa34eae75e3a9f3e82fa9bda82c48fcd159
Akos Marton <akos.marton@appian.com>
2023-10-13 23:14:05 +0200

    Small regex change.
    iex(7)> ExAws.Config.Defaults.host("kms", "us-east-1-fips")
    "kms.us-east-1-fips.amazonaws.com"
    
    iex(8)> ExAws.Config.Defaults.host("sts", "us-east-1-fips")
    "sts-fips.us-east-1.amazonaws.com"
    
    iex(9)> ExAws.Config.Defaults.host("s3", "us-east-1-fips")
    "s3.us-east-1-fips.amazonaws.com"
    
    iex(12)> ExAws.Config.Defaults.host("s3", "us-east-1")
    "s3.amazonaws.com"
    
    iex(13)> ExAws.Config.Defaults.host("sts", "us-east-1")
    "sts.us-east-1.amazonaws.com"
    
    iex(14)> ExAws.Config.Defaults.host("kms", "us-east-1")
    "kms.us-east-1.amazonaws.com"

--- lib/ex_aws/config/defaults.ex
+++ lib/ex_aws/config/defaults.ex
@@ -95,7 +95,7 @@ defmodule ExAws.Config.Defaults do
   end
 
   @partitions [
-    {~r/^(us|eu|af|ap|sa|ca|me)\-\w+-\d?-?\w+/, "aws"},
+    {~r/^(us|eu|af|ap|sa|ca|me)\-\w+-\d?-?\w+$/, "aws"},
     {~r/^cn\-\w+\-\d+$/, "aws-cn"},
     {~r/^us\-gov\-\w+\-\d+$/, "aws-us-gov"}
   ]
@@ -105,7 +105,6 @@ defmodule ExAws.Config.Defaults do
       Enum.find(@partitions, fn {regex, _} ->
         Regex.run(regex, region)
       end)
-      |> IO.inspect(label: "partition")
 
     with {_, partition} <- partition do
       do_host(partition, service, region)
