d5fe7612385a22c077302b63794fb38910abf971
Rodolfo Silva <contato@rodolfosilva.com>
2024-03-05 18:44:31 -0500

    Add `aoss` to endpoints.exs
    Add the OpenSearch Servelerss service to the endpoints list

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -684,6 +684,27 @@
             "us-west-2" => %{}
           }
         },
+        "aoss" => %{
+          "endpoints" => %{
+            "ap-northeast-1" => %{},
+            "ap-northeast-2" => %{},
+            "ap-east-1" => %{},
+            "ap-south-1" => %{},
+            "ap-southeast-1" => %{},
+            "ap-southeast-2" => %{},
+            "ca-central-1" => %{},
+            "eu-central-1" => %{},
+            "eu-west-1" => %{},
+            "eu-west-2" => %{},
+            "eu-west-3" => %{},
+            "eu-north-1" => %{},
+            "sa-east-1" => %{},
+            "us-east-1" => %{},
+            "us-east-2" => %{},
+            "us-west-1" => %{},
+            "us-west-2" => %{}
+          }
+        },
         "session.qldb" => %{
           "endpoints" => %{
             "ap-northeast-1" => %{},
@@ -2562,6 +2583,7 @@
           "endpoints" => %{"cn-north-1" => %{}, "cn-northwest-1" => %{}}
         },
         "es" => %{"endpoints" => %{"cn-northwest-1" => %{}}},
+        "aoss" => %{"endpoints" => %{"cn-northwest-1" => %{}}},
         "monitoring" => %{
           "defaults" => %{"protocols" => ["http", "https"]},
           "endpoints" => %{"cn-north-1" => %{}, "cn-northwest-1" => %{}}
@@ -2722,6 +2744,7 @@
           }
         },
         "es" => %{"endpoints" => %{"us-gov-east-1" => %{}, "us-gov-west-1" => %{}}},
+        "aoss" => %{"endpoints" => %{"us-gov-east-1" => %{}, "us-gov-west-1" => %{}}},
         "monitoring" => %{"endpoints" => %{"us-gov-east-1" => %{}, "us-gov-west-1" => %{}}},
         "elasticloadbalancing" => %{
           "endpoints" => %{
