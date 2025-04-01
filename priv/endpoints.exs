41fc3fdc0214cf4d99e3531b647c679d007c2e60
Lang Martin <lang.martin@121.health>
2025-04-01 13:48:51 -0400

    Add kinesisvideo region us-east-2

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -2437,8 +2437,9 @@ chime_voice_regions = [
           "endpoints" => %{
             "ap-northeast-1" => %{},
             "eu-central-1" => %{},
-            "eu-west-1" => %{},
             "us-east-1" => %{},
+            "us-east-2" => %{},
+            "eu-west-1" => %{},
             "us-west-2" => %{}
           }
         },
