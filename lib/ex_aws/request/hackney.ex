4df72be0cd22a915fd5502efc706348d48f4e07c
manuzimferreira <emanuelferreirafurtado@gmail.com>
2026-01-20 23:53:07 -0300

    fix(request): wrap HTTP adapters with Code.ensure_loaded? for optional deps

--- lib/ex_aws/request/hackney.ex
+++ lib/ex_aws/request/hackney.ex
@@ -1,32 +1,34 @@
-defmodule ExAws.Request.Hackney do
-  @behaviour ExAws.Request.HttpClient
+if Code.ensure_loaded?(:hackney) do
+  defmodule ExAws.Request.Hackney do
+    @behaviour ExAws.Request.HttpClient
 
-  @moduledoc """
-  Configuration for `:hackney`.
+    @moduledoc """
+    Configuration for `:hackney`.
 
-  Options can be set for `:hackney` with the following config:
+    Options can be set for `:hackney` with the following config:
 
-      config :ex_aws, :hackney_opts,
-        recv_timeout: 30_000
+        config :ex_aws, :hackney_opts,
+          recv_timeout: 30_000
 
-  The default config handles setting the above.
-  """
+    The default config handles setting the above.
+    """
 
-  @default_opts [recv_timeout: 30_000]
+    @default_opts [recv_timeout: 30_000]
 
-  def request(method, url, body \\ "", headers \\ [], http_opts \\ []) do
-    opts = Application.get_env(:ex_aws, :hackney_opts, @default_opts)
-    opts = http_opts ++ [:with_body | opts]
+    def request(method, url, body \\ "", headers \\ [], http_opts \\ []) do
+      opts = Application.get_env(:ex_aws, :hackney_opts, @default_opts)
+      opts = http_opts ++ [:with_body | opts]
 
-    case :hackney.request(method, url, headers, body, opts) do
-      {:ok, status, headers} ->
-        {:ok, %{status_code: status, headers: headers}}
+      case :hackney.request(method, url, headers, body, opts) do
+        {:ok, status, headers} ->
+          {:ok, %{status_code: status, headers: headers}}
 
-      {:ok, status, headers, body} ->
-        {:ok, %{status_code: status, headers: headers, body: body}}
+        {:ok, status, headers, body} ->
+          {:ok, %{status_code: status, headers: headers, body: body}}
 
-      {:error, reason} ->
-        {:error, %{reason: reason}}
+        {:error, reason} ->
+          {:error, %{reason: reason}}
+      end
     end
   end
 end
